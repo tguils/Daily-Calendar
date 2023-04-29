831070bc4cc6840e3702caf8a2d87175fe9f297c
LAPTOP-ELH40JFH\tessg <tess.guilmette@gmail.com>
2023-04-28 20:34:39 -0400

    adding if statements for time tracker

--- develop/script.js
+++ develop/script.js
@@ -2,124 +2,145 @@ dayjs.extend(window.dayjs_plugin_advancedFormat);
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var day = [
-  {
-    id: "0",
-    hour: "09",
-    time: "09",
-    meridiem: "am",
-    reminder: ""
-  },
-  {
-    id: "0",
-    hour: "09",
-    time: "09",
-    meridiem: "am",
-    reminder: ""
-  },
-  {
-    id: "0",
-    hour: "09",
-    time: "09",
-    meridiem: "am",
-    reminder: ""
-  },
-  {
-    id: "0",
-    hour: "09",
-    time: "09",
-    meridiem: "am",
-    reminder: ""
-  },
-  {
-    id: "0",
-    hour: "09",
-    time: "09",
-    meridiem: "am",
-    reminder: ""
-  },
-  {
-    id: "0",
-    hour: "09",
-    time: "09",
-    meridiem: "am",
-    reminder: ""
-  },
-  {
-    id: "1",
-    hour: "10",
-    time: "10",
-    meridiem: "am",
-    reminder: ""
-  },
-  {
-    id: "2",
-    hour: "11",
-    time: "11",
-    meridiem: "am",
-    reminder: ""
-  },
-  {
-    id: "3",
-    hour: "12",
-    time: "12",
-    meridiem: "pm",
-    reminder: ""
-  },
-  {
-    id: "4",
-    hour: "01",
-    time: "01",
-    meridiem: "pm",
-    reminder: ""
-  },
-  {
-    id: "5",
-    hour: "02",
-    time: "02",
-    meridiem: "pm",
-    reminder: ""
-  },
-  {
-    id: "6",
-    hour: "03",
-    time: "03",
-    meridiem: "pm",
-    reminder: ""
-  },
-  {
-    id: "7",
-    hour: "04",
-    time: "04",
-    meridiem: "pm",
-    reminder: ""
-  },
-  {
-    id: "8",
-    hour: "05",
-    time: "05",
-    meridiem: "pm",
-    reminder: ""
-  },
-  {
-    id: "9",
-    hour: "06",
-    time: "06",
-    meridiem: "pm",
-    reminder: ""
-  },
-]
-
+// var day = [
+//   {
+//     id: "0",
+//     hour: "09",
+//     time: "09",
+//     meridiem: "am",
+//     reminder: ""
+//   },
+//   {
+//     id: "0",
+//     hour: "09",
+//     time: "09",
+//     meridiem: "am",
+//     reminder: ""
+//   },
+//   {
+//     id: "0",
+//     hour: "09",
+//     time: "09",
+//     meridiem: "am",
+//     reminder: ""
+//   },
+//   {
+//     id: "0",
+//     hour: "09",
+//     time: "09",
+//     meridiem: "am",
+//     reminder: ""
+//   },
+//   {
+//     id: "0",
+//     hour: "09",
+//     time: "09",
+//     meridiem: "am",
+//     reminder: ""
+//   },
+//   {
+//     id: "0",
+//     hour: "09",
+//     time: "09",
+//     meridiem: "am",
+//     reminder: ""
+//   },
+//   {
+//     id: "1",
+//     hour: "10",
+//     time: "10",
+//     meridiem: "am",
+//     reminder: ""
+//   },
+//   {
+//     id: "2",
+//     hour: "11",
+//     time: "11",
+//     meridiem: "am",
+//     reminder: ""
+//   },
+//   {
+//     id: "3",
+//     hour: "12",
+//     time: "12",
+//     meridiem: "pm",
+//     reminder: ""
+//   },
+//   {
+//     id: "4",
+//     hour: "01",
+//     time: "01",
+//     meridiem: "pm",
+//     reminder: ""
+//   },
+//   {
+//     id: "5",
+//     hour: "02",
+//     time: "02",
+//     meridiem: "pm",
+//     reminder: ""
+//   },
+//   {
+//     id: "6",
+//     hour: "03",
+//     time: "03",
+//     meridiem: "pm",
+//     reminder: ""
+//   },
+//   {
+//     id: "7",
+//     hour: "04",
+//     time: "04",
+//     meridiem: "pm",
+//     reminder: ""
+//   },
+//   {
+//     id: "8",
+//     hour: "05",
+//     time: "05",
+//     meridiem: "pm",
+//     reminder: ""
+//   },
+//   {
+//     id: "9",
+//     hour: "06",
+//     time: "06",
+//     meridiem: "pm",
+//     reminder: ""
+//   },
+// ]
+var currentTime = dayjs().format('MMM DD, YYYY hh:mm:ss a');
 function runningTime() {
-  var currentDay = dayjs().format('ddd | MMM | YYYY');
-  $('#currentDay').text(currentDay);
+  $('#currentTime').text(currentTime);
 }
+var timeBlock = $(".time-block");
+function timeTrack() {
 
-function saveMeetings() {
-  localStorage.setitem("day", JSON.stringify(day));
-}
+
+  
+  if (timeBlock === currentTime) {
+    $(this).addClass('present');
+    $(this).removeClass('past');
+    $(this).removeClass('future');
+  }
+  else if (timeBlock < currentTime) {
+    $(this).addClass('past');
+    $(this).removeClass('present');
+    $(this).removeClass('future');
+  }
+  else {
+    $(this).addClass('future');
+    $(this).removeClass('present');
+    $(this).removeClass('past');
+  }
 
 }
+// function saveMeetings() {
+//   localStorage.setitem("day", JSON.stringify(day));
+// }
+
+$('#saveBtn').on('click', )
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
